fe3597228b138114af345ae5aeaa106baca9b816
dmitrylaskin <boombapkilla@yandex.ru>
2021-08-19 14:28:06 +0300

    fix server

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const middlewares = jsonServer.defaults({
     static: './build'
 })
 
-const PORT = process.env.PORT || 3004
+const PORT = process.env.PORT || 3001
 
 server.use(middlewares)
+server.use(jsonServer.rewriter({'/api/*': '/$1'}))
 server.use(router)
 
 server.listen(PORT, () => {
